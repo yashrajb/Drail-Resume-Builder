0e7d6ae9557bb049471e2ceb37664a77d515a3d5
yashrajb <imyashraj.basan@gmail.com>
2019-11-08 21:34:24 +0530

    changed header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./components/Header";
-import Form from "./components/Form";
+import Header from "./components/Header.js";
+import Form from "components/Form";
 import Resume from "./components/Resume";
 import store from "./store";
 import { Provider } from "react-redux";
