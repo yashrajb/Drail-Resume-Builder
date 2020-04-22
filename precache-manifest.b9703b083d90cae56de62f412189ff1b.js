c4162cbed31c4761a597e8013f5e2e38e551dfcf
yashrajb <imyashraj.basan@gmail.com>
2020-04-23 01:26:32 +0530

    Updates

---
+++ precache-manifest.b9703b083d90cae56de62f412189ff1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38d41499510351eab7b4fa975a2879b",
+    "url": "/drail-resume-builder/index.html"
+  },
+  {
+    "revision": "1673815edbdfcd00771c",
+    "url": "/drail-resume-builder/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5218d795bdc58ab7f595",
+    "url": "/drail-resume-builder/static/css/main.4c427d24.chunk.css"
+  },
+  {
+    "revision": "1673815edbdfcd00771c",
+    "url": "/drail-resume-builder/static/js/2.bcbbef30.chunk.js"
+  },
+  {
+    "revision": "5218d795bdc58ab7f595",
+    "url": "/drail-resume-builder/static/js/main.317dc4a7.chunk.js"
+  },
+  {
+    "revision": "d345008f0cbe56a8e89a",
+    "url": "/drail-resume-builder/static/js/runtime-main.31262196.js"
+  }
+]);
